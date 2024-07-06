4b027c7886f746cce096e56f03a7e7289da299e9
TomLeonhardt <tom-leonhardt@web.de>
2024-07-07 01:46:50 +0200

    Settigns geupdated wegen Demo Ansicht

--- settings.py
+++ settings.py
@@ -1,21 +1,28 @@
 from os import environ
-SESSION_CONFIG_DEFAULTS = dict(real_world_currency_per_point=1, participation_fee=0)
-SESSION_CONFIGS = [dict(name='cognitive_load_experiment', num_demo_participants=None, app_sequence=['cognitive_load_experiment'])]
+
+SESSION_CONFIGS = [
+    {
+        'name': 'cognitive_load_experiment',
+        'display_name': "Cognitive Load Experiment",
+        'num_demo_participants': 1,
+        'app_sequence': ['cognitive_load_experiment'],
+    },
+]
+
 LANGUAGE_CODE = 'en'
 REAL_WORLD_CURRENCY_CODE = 'USD'
 USE_POINTS = True
-DEMO_PAGE_INTRO_HTML = ''
-PARTICIPANT_FIELDS = []
-SESSION_FIELDS = []
-ROOMS = []
+
+ROOMS = [
+    {
+        'name': 'cognitive_room',
+        'display_name': 'Cognitive Experiment Room',
+    },
+]
 
 ADMIN_USERNAME = 'admin'
-# for security, best to set admin password in an environment variable
-ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD', 'your_default_password')
 
 SECRET_KEY = 'blahblah'
 
-# if an app is included in SESSION_CONFIGS, you don't need to list it here
-INSTALLED_APPS = ['otree']
-
-
+INSTALLED_APPS = ['otree', 'cognitive_load_experiment']
