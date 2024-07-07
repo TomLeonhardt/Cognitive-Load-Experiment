66cfbda0e38e29c9bea882caa79d0952cce9bbdf
TomLeonhardt <tom-leonhardt@web.de>
2024-07-07 02:02:28 +0200

    settignsn ochmal bearbeitet

--- settings.py
+++ settings.py
@@ -1,28 +1,29 @@
 from os import environ
 
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=1.00,
+    participation_fee=0.00,
+    doc=""
+)
+
 SESSION_CONFIGS = [
-    {
-        'name': 'cognitive_load_experiment',
-        'display_name': "Cognitive Load Experiment",
-        'num_demo_participants': 1,
-        'app_sequence': ['cognitive_load_experiment'],
-    },
+    dict(
+        name='cognitive_load_experiment',
+        display_name="Cognitive Load Experiment",
+        num_demo_participants=1,
+        app_sequence=['cognitive_load_experiment'],
+    ),
 ]
 
 LANGUAGE_CODE = 'en'
 REAL_WORLD_CURRENCY_CODE = 'USD'
 USE_POINTS = True
 
-ROOMS = [
-    {
-        'name': 'cognitive_room',
-        'display_name': 'Cognitive Experiment Room',
-    },
-]
+DEMO_PAGE_INTRO_HTML = ""
 
 ADMIN_USERNAME = 'admin'
-ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD', 'your_default_password')
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
 
 SECRET_KEY = 'blahblah'
 
-INSTALLED_APPS = ['otree', 'cognitive_load_experiment']
+INSTALLED_APPS = ['otree']
